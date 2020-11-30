d75e5559c63123c6db406380899c4a2aa2a60a0a
Aman Mac <singh_aman16@yahoo.com>
2020-11-30 22:46:23 +0530

    Change in Binding Variable
    Implemented cleaning of binding variable on destry view.

--- app/src/main/java/com/example/filetracker/filelist/FileListFragment.kt
+++ app/src/main/java/com/example/filetracker/filelist/FileListFragment.kt
@@ -11,13 +11,21 @@ import com.example.filetracker.databinding.FragmentFileListBinding
 import timber.log.Timber
 
 class FileListFragment: Fragment() {
+    private var _binding: FragmentFileListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         Timber.i("Creating View")
-        val binding = DataBindingUtil.inflate<FragmentFileListBinding>(inflater, R.layout.fragment_file_list, container, false)
+        _binding = DataBindingUtil.inflate<FragmentFileListBinding>(inflater, R.layout.fragment_file_list, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
